de61618cf4faef3dab72d238e461ad290d4a29a7
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-07-03 15:57:00 +0300

    first commit

---
+++ dz_js_1/script.js
@@ -0,0 +1,90 @@
+// /*
+// let a = 'hello';
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// let b = 'owu';
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// let c = 'com';
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// let d = 'ua';
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// let e = 1;
+// console.log(e);
+// alert(e);
+// document.write(e);
+//
+// let f = 10;
+// console.log(f);
+// alert(f);
+// document.write(f);
+//
+// let g = -999;
+// console.log(g);
+// alert(g);
+// document.write(g);
+//
+// let h = 123;
+// console.log(h);
+// alert(h);
+// document.write(h);
+//
+// let i = 3.14;
+// console.log(i);
+// alert(i);
+// document.write(i);
+//
+// let j = 2.7;
+// console.log(j);
+// alert(j);
+// document.write(j);
+//
+// let k = 16;
+// console.log(k);
+// alert(k);
+// document.write(k);
+//
+// let m = true;
+// console.log(m);
+// alert(m);
+// document.write(m);
+//
+// let n = false;
+// console.log(n);
+// alert(n);
+// document.write(n);
+//
+//
+// let firstName = 'Mykhailo';
+// let middleName = 'Myroslavovych';
+// let lastName = 'Zastavnyi';
+// let pip = firstName + " " + middleName + " " + lastName;
+// console.log(pip);
+//
+// let pip2 = `${firstName} ${middleName} ${lastName}`;
+// console.log(pip2);
+//
+//
+// let a1 = 100;
+// console.log(typeof a1);
+// let b1 = '100';
+// console.log(typeof b1);
+// let c1 = true;
+// console.log(typeof c1);
+//
+// let inputData = prompt("enter name");
+// console.log(!!inputData);
+// let inputData1 = prompt("enter middleName");
+// console.log(!!inputData1);
+// let inputData2 = +prompt("enter age");
+// console.log(!!inputData2);*/
---
+++ dz_js_2/script.js
@@ -0,0 +1,94 @@
+// Масиви та об'єкти:
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+// const arr = [1, 2, 'hello', true];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);console
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+// const book = {
+//     title: 'book',
+//     pageCount: 200,
+//     genre: 'comedy'
+// }
+// const book = {
+//     title: 'book',
+//     pageCount: 200,
+//     genre: 'comedy'
+// }
+// const book = {
+//     title: 'book',
+//     pageCount: 200,
+//     genre: 'comedy'
+// }
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - являється
+// масивом. Кожен автор має поля name та age.
+
+// const book = {
+//     title: 'book',
+//     pageCount: 200,
+//     genre: 'comedy',
+//     authors: [
+//         {
+//             name: "Vasya",
+//             age:22
+//         },
+//         {
+//             name: "Petya",
+//             age: 33
+//         }
+//     ]
+// }
+// const book = {
+//     title: 'book',
+//     pageCount: 200,
+//     genre: 'comedy',
+//     authors: [
+//         {
+//             name: "Vasya",
+//             age:22
+//         },
+//         {
+//             name: "Petya",
+//             age: 33
+//         }
+//     ]
+// }
+// const book = {
+//     title: 'book',
+//     pageCount: 200,
+//     genre: 'comedy',
+//     authors: [
+//         {
+//             name: "Vasya",
+//             age:22
+//         },
+//         {
+//             name: "Petya",
+//             age: 33
+//         }
+//     ]
+// }
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль ' +
+// 'пароль кожного користувача
+
+// let arr = [
+//     {name: 'ivan', username: 'st22', password: 2222},
+//     {name: 'roman', username: 'wwsq', password: 2311},
+//     {name: 'yura', username: 'erwq', password: 3422},
+//     {name: 'katya',username: 'gghd', password: 7767},
+//     {name: 'olya', username: 'ffff', password: 4433},
+//     {name: 'stepan', username: 'klaa', password: 3399},
+//     {name: 'kolya', username: 'bnnn', password: 3322},
+//     {name: 'sasha', username: 'op33', password: 2233},
+//     {name: 'vova', username: 'rr11',password: 5777},
+//     {name: 'ira',username: 'po00', password: 8991},
+// ];
+//
+// console.log(arr[5].password);
+
